b08cb4847c3db148e7918b909d4dbf79850e4511
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2026-01-21 21:22:05 +0200

    Update ProjectCard.js

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,27 +1,37 @@
 import { Col } from "react-bootstrap";
 
-export const ProjectCard = ({ title, description, imgUrl, link }) => {
+export const ProjectCard = ({ title, description, imgUrl, link, demo }) => {
   return (
-    <Col sm={6} md={4}>
+    <Col size={12} sm={6} md={4}>
       <div className="proj-imgbx">
-        <img src={imgUrl} alt="" />
+        <img src={imgUrl} alt={title} />
         <div className="proj-txtx">
           <h4>{title}</h4>
-          <span>{description}</span>
-          <br />
-          {link && (
-            <a
-              href={link}
-              target="_blank"
-              rel="noreferrer"
-              style={{
-                textDecoration: "none",
-                color: "black",
-              }}
-            >
-              github link
-            </a>
-          )}
+          <p>{description}</p>
+
+          <div className="project-links">
+            {link && (
+              <a
+                href={link}
+                target="_blank"
+                rel="noreferrer"
+                className="project-btn github"
+              >
+                GitHub
+              </a>
+            )}
+
+            {demo !== undefined && (
+              <a
+                href={demo || "#"}
+                target="_blank"
+                rel="noreferrer"
+                className="project-btn demo"
+              >
+                Live Demo
+              </a>
+            )}
+          </div>
         </div>
       </div>
     </Col>
