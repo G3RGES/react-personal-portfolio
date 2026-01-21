446609b6a6f300a17a894c9cdbd0ee9dce82d676
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2026-01-21 22:02:13 +0200

    Update ProjectCard.js

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,14 +1,32 @@
 import { Col } from "react-bootstrap";
+import { FaGithub, FaExternalLinkAlt } from "react-icons/fa";
 
-export const ProjectCard = ({ title, description, imgUrl, link, demo }) => {
+export const ProjectCard = ({
+  title,
+  description,
+  imgUrl,
+  link,
+  demo,
+  stack = [],
+}) => {
   return (
     <Col size={12} sm={6} md={4}>
       <div className="proj-imgbx">
         <img src={imgUrl} alt={title} />
-        <div className="proj-txtx">
+
+        <div className="proj-overlay">
           <h4>{title}</h4>
+
           <p>{description}</p>
 
+          {/* Tech Stack */}
+          <div className="tech-stack">
+            {stack.map((tech, i) => (
+              <span key={i}>{tech}</span>
+            ))}
+          </div>
+
+          {/* Buttons */}
           <div className="project-links">
             {link && (
               <a
@@ -17,20 +35,19 @@ export const ProjectCard = ({ title, description, imgUrl, link, demo }) => {
                 rel="noreferrer"
                 className="project-btn github"
               >
-                GitHub
+                <FaGithub /> GitHub
               </a>
             )}
 
-            {demo !== undefined && (
-              <a
-                href={demo || "#"}
-                target="_blank"
-                rel="noreferrer"
-                className="project-btn demo"
-              >
-                Live Demo
-              </a>
-            )}
+            <a
+              href={demo || "#"}
+              target="_blank"
+              rel="noreferrer"
+              className={`project-btn demo ${!demo ? "disabled" : ""}`}
+              onClick={(e) => !demo && e.preventDefault()}
+            >
+              <FaExternalLinkAlt /> Live Demo
+            </a>
           </div>
         </div>
       </div>
